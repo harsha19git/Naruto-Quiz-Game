9e3b19f4be1c1bebc064f6b44f40b0cb96c9662e
harsha19git <96284430+harsha19git@users.noreply.github.com>
2022-02-02 16:32:36 +0530

    Genin level

--- academy.js
+++ academy.js
@@ -1,216 +1,89 @@
 // Question 1
-var num = 0; 
-function goGreen1() {
-    let a = document.getElementsByClassName("correct1")[0];
+var correctAns = 0; 
+var incorrectAns = 0;
+var assign = 0;
+function changeColor (correct , inc , q , isGreen) {
+    assign++;
+    let a = document.getElementById(correct);
     a.remove();
     
-    let b = document.getElementsByClassName("inc1");
+    let b = document.getElementsByClassName(inc);
     i = 0;
     while(b.length != 0) {
         b[i].remove();
     }
-    let c = document.getElementsByClassName("q1");
+    let c = document.getElementsByClassName(q);
 
-
-    let d = document.createElement('span');
+  let d = document.createElement('span');
+  if(isGreen) {
     d.innerHTML="Yohoo..Correct!";
-    d.classList.add("add");
+    d.style. boxShadow= "10px 10px 50px yellowGreen";
+    d.classList.add("goGreen");
+    correctAns++;
+    if(correctAns == 3) {
+        alert("Congratulations! you made it to Genin level");
+  
+    }
+  }
+  else {
+    d.innerHTML="Woopsiee..Wrong!";
+    d.style. boxShadow= "10px 10px 80px rgb(253, 5, 5)";
+    d.classList.add("goRed"); 
+    incorrectAns++;
+    if(incorrectAns == 3)
+    alert("Give it another try! You'll get there.");
+
+    
+  }
+
+  
+
+
+  if(assign == 5) {
+      teamLeader();
+  }
+    
     c[0].appendChild(d);
     num++;
 }
 
 
 
-function goRed1() {
-    let a = document.getElementsByClassName("correct1")[0];
-    a.remove();
-    
-    let b = document.getElementsByClassName("inc1");
-    i = 0;
-    while(b.length != 0) {
-        b[i].remove();
+let counter = 0;
+function count() {
+    counter++
+   let x = document.body;
+    if (counter == 1) {
+        x.style.backgroundColor="black";
     }
-    let c = document.getElementsByClassName("q1");
-    let d = document.createElement('span');
-    d.innerHTML="Woopsiee..Wrong!";
-    d.classList.add("add1");
-    c[0].appendChild(d);
 
-}
-
-// Question 2
-
-function goGreen2() {
-    let a = document.getElementsByClassName("correct2")[0];
-    a.remove();
-    
-    let b = document.getElementsByClassName("inc2");
-    i = 0;
-    while(b.length != 0) {
-        b[i].remove();
+    if(counter == 2) 
+    {
+        x.style.backgroundColor="rgb(214, 95, 59)";
+        counter = 0;   
     }
-    let c = document.getElementsByClassName("q2");
-
-
-    let d = document.createElement('span');
-    d.innerHTML="Yohoo..Correct!";
-    d.classList.add("add");
-    c[0].appendChild(d);
-    num++;
+   
 }
 
 
 
-function goRed2() {
-    let a = document.getElementsByClassName("correct2")[0];
-    a.remove();
+
+function teamLeader() {
     
-    let b = document.getElementsByClassName("inc2");
-    i = 0;
-    while(b.length != 0) {
-        b[i].remove();
+    if(correctAns == 5) {
+        alert("Congratulations! Your Team leader is Kakashi Hatake!");
     }
-    let c = document.getElementsByClassName("q2");
-    let d = document.createElement('span');
-    d.innerHTML="Woopsiee..Wrong!";
-    d.classList.add("add1");
-    c[0].appendChild(d);
-    
-}
 
-
-// Question 3 
-function goGreen3() {
-    let a = document.getElementsByClassName("correct3")[0];
-    a.remove();
-    
-    let b = document.getElementsByClassName("inc3");
-    i = 0;
-    while(b.length != 0) {
-        b[i].remove();
+    if(correctAns == 4 && incorrectAns == 1) {
+        alert("Congratulations! Your Team leader is Asuma Sarutobi!");
     }
-    let c = document.getElementsByClassName("q3");
 
-
-    let d = document.createElement('span');
-    d.innerHTML="Yohoo..Correct!";
-    d.classList.add("add");
-    c[0].appendChild(d);
-    num++;
-}
-
-
-
-function goRed3() {
-    let a = document.getElementsByClassName("correct3")[0];
-    a.remove();
-    
-    let b = document.getElementsByClassName("inc3");
-    i = 0;
-    while(b.length != 0) {
-        b[i].remove();
-    }
-    let c = document.getElementsByClassName("q3");
-    let d = document.createElement('span');
-    d.innerHTML="Woopsiee..Wrong!";
-    d.classList.add("add1");
-    c[0].appendChild(d);
-
-}
-
-// Question 4
-
-
-function goGreen4() {
-    let a = document.getElementsByClassName("correct4")[0];
-    a.remove();
-    
-    let b = document.getElementsByClassName("inc4");
-    i = 0;
-    while(b.length != 0) {
-        b[i].remove();
-    }
-    let c = document.getElementsByClassName("q4");
-
-
-    let d = document.createElement('span');
-    d.innerHTML="Yohoo..Correct!";
-    d.classList.add("add");
-    c[0].appendChild(d);
-    num++;
-}
-
-
-
-function goRed4() {
-    let a = document.getElementsByClassName("correct4")[0];
-    a.remove();
-    
-    let b = document.getElementsByClassName("inc4");
-    i = 0;
-    while(b.length != 0) {
-        b[i].remove();
-    }
-    let c = document.getElementsByClassName("q4");
-    let d = document.createElement('span');
-    d.innerHTML="Woopsiee..Wrong!";
-    d.classList.add("add1");
-    c[0].appendChild(d);
-
-}
-
-
-//  Question 5
-
-
-function goGreen5() {
-    let a = document.getElementsByClassName("correct5")[0];
-    a.remove();
-    
-    let b = document.getElementsByClassName("inc5");
-    i = 0;
-    while(b.length != 0) {
-        b[i].remove();
-    }
-    let c = document.getElementsByClassName("q5");
-
-
-    let d = document.createElement('span');
-    d.innerHTML="Yohoo..Correct!";
-    d.classList.add("add");
-    c[0].appendChild(d);
-    num++;
-    check();
-}
-
-
-
-function goRed5() {
-    let a = document.getElementsByClassName("correct5")[0];
-    a.remove();
-    
-    let b = document.getElementsByClassName("inc5");
-    i = 0;
-    while(b.length != 0) {
-        b[i].remove();
-    }
-    let c = document.getElementsByClassName("q5");
-    let d = document.createElement('span');
-    d.innerHTML="Woopsiee..Wrong!";
-    d.classList.add("add1");
-    c[0].appendChild(d);
-    check();
-}
-
-function check() {
-    if(num >= 3) 
-    alert("Congratulations! you made it to Genin level");
-
-    else {
-        alert("Give it another try! You'll get there.");
+    if(correctAns == 3  && incorrectAns == 2) {
+        alert("Congratulations! Your Team leader is Mighty Guy!");
     }
 
 }
+
 
 
 
---
+++ genin.js
@@ -0,0 +1,103 @@
+function convert(q , aq) {
+    let x = document.getElementById(q);
+    x.remove();
+    let y = document.getElementById(aq);
+    y.style.display="block";
+    y.style.marginLeft="525px";
+    y.style.marginBottom="45px";
+    y.style.marginTop="45px";
+
+    if(aq == 'aq1') {
+        y.style.marginTop="500px";  
+    }
+    
+} 
+ let counter = 0;
+function count() {
+    counter++
+  
+    let x = document.body;
+    if(counter == 1) {
+        x.style.backgroundColor="yellowGreen";
+    }
+   
+    if (counter == 2) {
+        x.style.backgroundColor="rgb(30, 104, 98)";
+    }
+
+    if (counter == 3) {
+        x.style.backgroundColor="black";
+    }
+
+    if (counter == 4) {
+        x.style.backgroundColor="rgb(138, 31, 31)"; 
+        counter = 0;
+    }
+  
+    
+    
+}
+
+
+var correctAns = 0; 
+var incorrectAns = 0;
+let assign = 0;
+function changeColor (correct , inc , q , isGreen) {
+     assign++;
+    
+    let a = document.getElementById(correct);
+    a.remove();
+    
+    let b = document.getElementsByClassName(inc);
+    i = 0;
+    while(b.length != 0) {
+        b[i].remove();
+    }
+    let c = document.getElementById(q);
+
+  let d = document.createElement('span');
+  if(isGreen) {
+    d.innerHTML="Yohoo..Correct!";
+    d.style. boxShadow= "10px 10px 80px yellowGreen";
+    d.classList.add("goGreen");
+    correctAns++;
+    if(correctAns == 3) 
+    alert("Congratulations! you made it to Jonin level");
+   
+
+  }
+  else {
+    d.innerHTML="Woopsiee..Wrong!";
+    d.style. boxShadow= "10px 10px 80px rgb(253, 5, 5)";
+    d.classList.add("goRed"); 
+    incorrectAns++;
+    if(incorrectAns == 3)
+    alert("Give it another try! You'll get there.");
+
+    
+  }
+
+  if(assign == 5) {
+    teamLeader(assign);
+}
+    
+    c.appendChild(d);
+    num++;
+
+}
+
+function teamLeader() {
+    
+    if( correctAns == 5) {
+        alert("Congratulations! Your Team leader is Jiraiya!");
+    }
+
+    if(correctAns == 4 && incorrectAns == 1) {
+        alert("Congratulations! Your Team leader is Tsunade!");
+    }
+
+    if(correctAns == 3  && incorrectAns == 2) {
+        alert("Congratulations! Your Team leader is Orochimaru!");
+    }
+
+}
